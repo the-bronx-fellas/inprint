b6da822478945398b7cfda24520f841d49a50cd8
Freddy Gomez <fjohngomez90@gmail.com>
2022-06-26 08:21:33 -0400

    create working

--- client/src/hooks/inPrintHooks.js
+++ client/src/hooks/inPrintHooks.js
@@ -2,7 +2,7 @@ import { Blog } from './inprint';
 
 export const connectBlog = () => {
   const blog = new Blog("http://127.0.0.1:8545");
-  blog.connectToBlogAddress('0x5FbDB2315678afecb367f032d93F642f64180aa3');
+  blog.connectToBlogAddress('0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0');
 
-  return blog;
+  return blog ;
 }
