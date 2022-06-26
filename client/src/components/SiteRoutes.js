5c6ae5d697517b49ccef10ae952efa1ae9f8c710
Freddy Gomez <fjohngomez90@gmail.com>
2022-06-26 00:21:12 -0400

    Router updated with View/create on welcome page

--- client/src/components/SiteRoutes.js
+++ client/src/components/SiteRoutes.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom'
 import View from './View'
+import Welcome from './Welcome'
 
 const SiteRoutes = () => {
 
@@ -8,6 +9,7 @@ const SiteRoutes = () => {
     <>
       <Routes>
         <Route path="/view" element={<View />} />
+        <Route path="*" element={<Welcome />} />
       </Routes>
     </>
   )
